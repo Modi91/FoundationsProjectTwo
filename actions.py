63117e3291df264ca13de197046c691cdc4382f1
Apple <apple@Apples-MacBook.local>
2019-02-07 13:25:37 +0300

    stuff

--- actions.py
+++ actions.py
@@ -1,8 +1,8 @@
 # UTILS AND FUNCTIONALITY
 from data import stores
-from components import Cart
+from components import Cart, Store
 
-site_name = ""  # Give your site a name
+site_name = "Mood"  # Give your site a name
 
 def welcome():
     print("Welcome to %s\nFeel free to shop throughout the stores we have, and only checkout once!" % site_name)
@@ -11,32 +11,59 @@ def print_stores():
     """
     prints the list of stores in a nice readable format.
     """
-    # your code goes here!
+    for store in stores:
+        print(store)
+    
 
 def get_store(store_name):
     """
     receives a name for a store, and returns the store object with that name.
     """
-    # your code goes here!
+    for store in stores:
+        if store.name == store_name:
+          return store
+    return False
 
 def pick_store():
     """
     prints list of stores and prompts user to pick a store.
     """
-    # your code goes here!
+    print_stores()
+    my_store = input("Pick a store by typing its name. Or type 'checkout' to pay your bills.")
+    while my_store.lower() != 'checkout':
+        if get_store(my_store):
+            #my_store.pick_products()
+            return get_store(my_store)
+        else: 
+          print("Invalid! Please try again..")
+        break
+    pick_store()
 
 def pick_products(cart, picked_store):
     """
     prints list of products and prompts user to add products to card.
     """
-    # your code goes here!
+    picked_store.print_products()
+
+
+    my_product = input("What would you like to add to the cart? ")
+    while my_product.lower() != 'back':
+        for product in picked_store.products:
+            if product.name == my_product:
+                cart.add_to_cart(product)
+
+        my_product = input("What else would you like? ")
 
 def shop():
     """
     The main shopping functionality
     """
     cart = Cart()
-    # your code goes here!
+    store = pick_store()
+    #pick_store()
+    while store != 'checkout':
+        pick_products(cart, store)
+    cart.checkout()
 
 def thank_you():
     print("Thank you for shopping with us at %s" % site_name)
